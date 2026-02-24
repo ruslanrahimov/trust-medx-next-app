6269f92d9835281d6b7c3cd0acf2701b1a26bf54
Ruslan Rahimov <rahimoff.tm@gmail.com>
2026-02-24 17:11:16 +0300

    add workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const hasRepoName = repoName.length > 0;
+const basePath = isGithubActions && hasRepoName ? `/${repoName}` : "";
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
